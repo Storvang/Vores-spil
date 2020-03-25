4b4846ff6187c6869d7ec0c71fd923c49d45f17c
Rasmus L. Haun <rasm710k@edu.aarhustech.dk>
2020-03-25 19:06:16 +0100

    Added cloud.1-3 Koder tilhørende ikke tilføjet (virkede ikke)

---
+++ Baggrund.py
@@ -0,0 +1,3 @@
+Cloud1 = pygame.image.load(os.path.join("Assets", "Baggrund", "Cloud_1.png"))
+Cloud1 = pygame.image.load(os.path.join("Assets", "Baggrund", "Cloud_2.png"))
+Cloud1 = pygame.image.load(os.path.join("Assets", "Baggrund", "Cloud_3.png"))
